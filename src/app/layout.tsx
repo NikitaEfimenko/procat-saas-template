6c69e3cdfe1375729e3271dc418b07fd02f5e075
Regelity <mszx2000@gmail.com>
2025-02-21 22:12:09 +0300

    fix switch token

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import Link from "next/link";
+import { AutoAuth } from "./sdk/components/auto-auth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
+          <AutoAuth
+            loader={<div className="ring w-8 h-8 animate-spin" />}
+          />
           <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 justify-center items-center">
               {children}
---
+++ src/app/sdk/components/auto-auth.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { signIn, useSession } from "next-auth/react"
+import { ReactNode, useEffect, useState } from "react"
+
+type AuthAuthProps = {
+  loader?: ReactNode
+}
+
+export const AutoAuth = ({
+  loader = <p>loading...</p>
+}: AuthAuthProps) => {
+  const [loading, setLoading] = useState(true)
+  const { status } = useSession();
+
+  useEffect(() => {
+    const trySignIn = async () => {
+      const webapp = (window as any)?.Telegram?.WebApp
+      if (webapp && webapp?.initData && status === "unauthenticated") {
+        await signIn('procat', undefined, webapp.initData)
+      }
+      setLoading(false)
+    }
+    trySignIn()
+  }, [status])
+
+  if (status === "authenticated") {
+    return null;
+  }
+
+  return loading ? <div className="bg-black w-full h-full z-50 absolute flex flex-col items-center justify-center">
+    {loader}
+  </div> : null
+}

5c9a3859ed467e1add071821787a8912fdbe5e56
Regelity <mszx2000@gmail.com>
2025-02-22 01:21:38 +0300

    fix switch token

--- src/app/sdk/helpers.ts
+++ src/app/sdk/helpers.ts
@@ -23,28 +23,39 @@ export function nullguard<V>(v: V, render: (v: NonNullable<V>) => React.ReactNod
 }
 
 export const getInitDataFromUrl = (): string | null => {
-  let initData = (window as any)?.Telegram?.WebApp?.initData;
+  if (typeof window !== "undefined") {
+    let initData = (window as any)?.Telegram?.WebApp?.initData;
 
-  if (!initData) {
-    const params = new URLSearchParams(window.location.search);
-    initData = params.get('initData');
-    
-    if (initData) {
-      initData = decodeURIComponent(initData);
+    if (!initData) {
+      const params = new URLSearchParams(window.location.search);
+      initData = params.get('initData');
+      
+      if (initData) {
+        initData = decodeURIComponent(initData);
+      }
     }
-  }
 
-  return initData;
+    return initData;
+  }
+  
+  // Возвращаем null, если код выполняется на сервере
+  return null;
 };
 
 export const extendWithInitData = (url: string): string => {
-  const webapp = (window as any)?.Telegram?.WebApp;
-  let initData = webapp?.initData;
-  if (initData) {
-    const encodedInitData = encodeURIComponent(initData);
-    const urlObj = new URL(url);
-    urlObj.searchParams.set('initData', encodedInitData);
-    return urlObj.toString();
+  if (typeof window !== "undefined") {
+    const webapp = (window as any)?.Telegram?.WebApp;
+    let initData = webapp?.initData;
+
+    if (initData) {
+      const encodedInitData = encodeURIComponent(initData);
+      const urlObj = new URL(url);
+      urlObj.searchParams.set('initData', encodedInitData);
+      return urlObj.toString();
+    }
   }
+
+  // Возвращаем оригинальный URL, если код выполняется на сервере или нет initData
   return url;
 };
+
